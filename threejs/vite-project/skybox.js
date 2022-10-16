6e62392111adc60dd03f69dc9d60d0b59d596ce2
Izhan Nadeem <57583461+1zhanN@users.noreply.github.com>
2022-10-16 19:10:17 +0500

    Add files via upload

--- threejs/vite-project/skybox.js
+++ threejs/vite-project/skybox.js
@@ -1,11 +1,15 @@
 // npm run dev for local server
 // ctrl + C to close local server
-let scene, camera, renderer;
+var scene, camera, renderer, orient;
 
 function init() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
-  camera.position.set(200,150,0);
+  camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight,45,30000);
+  camera.position.set(0,0,100);
+  orient = new THREE.Vector3(0,0,0)
+  
+  
+
     
   renderer = new THREE.WebGLRenderer({
     antialias:true,
@@ -24,33 +28,81 @@ function init() {
   let texture_lf = new THREE.TextureLoader().load("material/Daylight Box_Left.bmp");
 
 
-    
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft,
+    wireframe : false  }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk,
+    wireframe : false  }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up,
+    wireframe : false  }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn,
+    wireframe : false  }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt,
+    wireframe : false  }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf,
+    wireframe : false  }));
+
+
+
 
   for (let i = 0; i < 6; i++)
      materialArray[i].side = THREE.BackSide;
-  var skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000);
-  var skybox = new THREE.Mesh( skyboxGeo, materialArray );
-
+  var skyboxGeo = new THREE.BoxGeometry(400, 400, 400);
+  var skybox = new THREE.Mesh(skyboxGeo, materialArray);
   scene.add(skybox);  
  
+  
 }
+
+window.onkeydown = function (e){ 
+
+  if (e.key == "a"){
+    camera.position.x -= 2
+  }
+  else if (e.key == "d"){
+    camera.position.x += 2
+  }
+  else if (e.key == "w"){
+    camera.position.z -= 2
+  }
+  else if (e.key == "s"){
+    camera.position.z += 2
+  }
+  else if (e.key == "q"){
+    camera.position.y += 2
+  }
+  else if (e.key == "e"){
+    camera.position.y -= 2
+  }
+
+  else if (e.key == "ArrowRight"){
+    orient.x += 5
+    camera.lookAt(orient);    
+  }
+  else if (e.key == "ArrowLeft"){
+    orient.x -= 5
+    camera.lookAt(orient);
+  }
+  else if (e.key == "ArrowUp"){
+    orient.y += 5
+    camera.lookAt(orient);
+  }
+  else if (e.key == "ArrowDown"){
+    orient.y -= 5
+    camera.lookAt(orient);
+
+  }
+console.log(orient)
+
+
+}
+
 
 init();
 
 function animate() {
-    
-    camera.rotation.y += 0.001;
-
   renderer.render(scene,camera);
   requestAnimationFrame(animate);
 }
+
 animate();
-
-
-
